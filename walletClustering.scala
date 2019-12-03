e61d0e5e08ba652dac390b0758e653792515e589
jjohnson9899 <42598050+jjohnson9899@users.noreply.github.com>
2019-12-02 18:11:42 -0700

    bracket issue

--- walletClustering.scala
+++ walletClustering.scala
@@ -20,29 +20,29 @@ object walletClustering{
 		write key pair adress and cluster to file
 		*/
 	}
-}
+	
 //inputs either "input"/"output", timestamp, val
 //returns out key = key, value = inputTotal, ouputTotal, numTransactions
-def joinPrep(key:String, value:String): String ={
-	val val_arr = value.split(", ")
-	if(val_arr(0) == "input"){
-		return "%s, 0, 1".format(val_arr(2))
+	def joinPrep(key:String, value:String): String ={
+		val val_arr = value.split(", ")
+		if(val_arr(0) == "input"){
+			return "%s, 0, 1".format(val_arr(2))
+		}
+		else{
+			return "0, %s, 1".format(val_arr(2))
+		}
 	}
-	else{
-		return "0, %s, 1".format(val_arr(2))
-	}
-}
 
 //inputs key = key, value = inputTotal, ouputTotal, numTransactions
 //returns out key = key, value = inputTotal, ouputTotal, numTransactions
-def joinFunc(accum:String, value:String): String ={
+	def joinFunc(accum:String, value:String): String ={
 	//split inputs into parts
-	val accum_arr = accum.split(", ").map(v => v.toLong)
-	val val_arr = value.split(", ").map(v => v.toLong)
+		val accum_arr = accum.split(", ").map(v => v.toLong)
+		val val_arr = value.split(", ").map(v => v.toLong)
 	//add components
-	val result = accum_arr.zip(val_arr).map{case (x,y) => x+y}
-	return result.mkString(", ")
-}
+		val result = accum_arr.zip(val_arr).map{case (x,y) => x+y}
+		return result.mkString(", ")
+	}
 /*
 def shiftFunc{
 	tempValues = values
@@ -64,3 +64,4 @@ def notClustered{
 	what do we need for this old and new point locations
 	can we pull out points that have stopped moving?
 }*/
+}
