c1c0325ef625262f22b746f39520d10dfbc7e4ee
jjohnson9899 <42598050+jjohnson9899@users.noreply.github.com>
2019-12-05 19:44:15 -0700

    Update walletGatherer.sh

--- walletGatherer.sh
+++ walletGatherer.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-wget https://www.walletexplorer.com/wallet/Bittrex.com?format=csv -O ->> exchanges.csv
+wget https://www.walletexplorer.com/wallet/Bittrex.com?format=csv -O ->> exchanges.txt
 
-wget https://www.walletexplorer.com/wallet/BTCCPool?format=csv -O ->> pool.csv
+wget https://www.walletexplorer.com/wallet/BTCCPool?format=csv -O ->> pool.txt
 
-wget https://www.walletexplorer.com/wallet/Xapo.com?format=csv -O ->> services.csv
+wget https://www.walletexplorer.com/wallet/Xapo.com?format=csv -O ->> services.txt
 
-wget https://www.walletexplorer.com/wallet/LuckyB.it?format=csv -O ->> gambling.csv
+wget https://www.walletexplorer.com/wallet/LuckyB.it?format=csv -O ->> gambling.txt
